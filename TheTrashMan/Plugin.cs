b520bc2a71de9c3f61140557b88e51ab35cd5217
dcljr <dcljr@me.com>
2019-10-15 22:24:35 -0700

    inital commit

---
+++ TheTrashMan/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BeatSaberMarkupLanguage.Settings;
+using IPA;
+using System;
+using System.Runtime;
+using UnityEngine.SceneManagement;
+using UnityEngine.Scripting;
+
+namespace TheTrashMan
+{
+    public class Plugin : IBeatSaberPlugin
+    {
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+            if (nextScene.name == "MenuCore")
+            {
+                BSMLSettings.instance.AddSettingsMenu("The Trash Man", "TheTrashMan.Views.settings.bsml", Settings.instance);
+                GarbageCollector.GCMode = GarbageCollector.Mode.Enabled;
+                GCSettings.LatencyMode = Settings.instance.MenuMode;
+            }
+            if (nextScene.name == "GameCore")
+            {
+                if (Settings.instance.DisableInGameCore)
+                    GarbageCollector.GCMode = GarbageCollector.Mode.Disabled;
+                GCSettings.LatencyMode = Settings.instance.GameCoreMode;
+            }
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) { }
+        public void OnApplicationStart() { }
+        public void OnApplicationQuit() { }
+        public void OnSceneUnloaded(Scene scene) { }
+        public void OnUpdate() { }
+        public void OnFixedUpdate() { }
+    }
+}
---
+++ TheTrashMan/Settings.cs
@@ -0,0 +1,83 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Parser;
+using BS_Utils.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheTrashMan
+{
+    class Settings : PersistentSingleton<Settings>
+    {
+        private Config config;
+
+        [UIParams]
+        private BSMLParserParams parserParams;
+
+        [UIValue("modes")]
+        private List<object> options = (new object[]{ GCLatencyMode.Batch, GCLatencyMode.Interactive, GCLatencyMode.LowLatency, GCLatencyMode.SustainedLowLatency }).ToList();
+
+        [UIValue("disable-in-gamecore")]
+        public bool DisableInGameCore
+        {
+            get => config.GetBool("General", "Disable During Gameplay", false);
+            set => config.SetBool("General", "Disable During Gameplay", value);
+        }
+
+        private GCLatencyMode gamecoreMode;
+        [UIValue("gamecore-mode")]
+        public GCLatencyMode GameCoreMode
+        {
+            get => gamecoreMode;
+            set
+            {
+                gamecoreMode = value;
+                config.SetString("Modes", "Gameplay", gamecoreMode.ToString());
+            }
+        }
+
+        private GCLatencyMode menuMode;
+        [UIValue("menu-mode")]
+        public GCLatencyMode MenuMode
+        {
+            get => menuMode;
+            set
+            {
+                menuMode = value;
+                config.SetString("Modes", "Menu", menuMode.ToString());
+            }
+        }
+        
+        public void Awake()
+        {
+            config = new Config("The Trash Man");
+            if (Enum.TryParse(config.GetString("Modes", "Gameplay", "LowLatency"), out GCLatencyMode gamecoreParsed))
+                gamecoreMode = gamecoreParsed;
+            else
+                gamecoreMode = GCLatencyMode.LowLatency;
+            if (Enum.TryParse(config.GetString("Modes", "Menu", "Interactive"), out GCLatencyMode menuParsed))
+                menuMode = menuParsed;
+            else
+                menuMode = GCLatencyMode.Interactive;
+        }
+        [UIAction("recommended-click")]
+        public void RecommendedPreset()
+        {
+            DisableInGameCore = false;
+            GameCoreMode = GCLatencyMode.LowLatency;
+            MenuMode = GCLatencyMode.Interactive;
+            parserParams.EmitEvent("cancel");
+        }
+        [UIAction("default-click")]
+        public void DefaultPreset()
+        {
+            DisableInGameCore = false;
+            GameCoreMode = GCLatencyMode.Interactive;
+            MenuMode = GCLatencyMode.Interactive;
+            parserParams.EmitEvent("cancel");
+        }
+    }
+}
